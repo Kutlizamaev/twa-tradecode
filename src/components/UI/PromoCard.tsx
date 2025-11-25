444a76ebc115111b2207d98a0dfc9b43eb5a60fc
Фаниль <71935758+Kutlizamaev@users.noreply.github.com>
2025-11-25 16:53:13 +0500

    add promo card link

--- src/components/UI/PromoCard.tsx
+++ src/components/UI/PromoCard.tsx
@@ -2,8 +2,19 @@ import styles from './UIStyles.module.css'
 import PromoCardImagePng from '../../assets/icons/ui/PromoCardImage.png'
 
 const PromoCard = () => {
+    const openTelegramPost = (link: string) => {
+        if (window?.Telegram?.WebApp?.openTelegramLink) {
+            window.Telegram.WebApp.openTelegramLink(link)
+        } else {
+            window.open(link, '_blank')
+        }
+    }
+
     return (
-        <article className={styles.promoCard}>
+        <article
+            onClick={() => openTelegramPost('https://t.me/p2p_blacklist')}
+            className={styles.promoCard}
+        >
             <img src={PromoCardImagePng} alt="Promo Card" width={'100%'} />
         </article>
     )
